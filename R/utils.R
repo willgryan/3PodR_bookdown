748a6204a541afec1ae37fbee46d4dca3195db12
William Ryan <billyryan543@gmail.com>
2023-06-20 19:16:49 -0400

    Update Enrichr GO dbs.

--- R/utils.R
+++ R/utils.R
@@ -7,9 +7,9 @@ quiet <- function(x) {
 #Input: Vector of Gene symbols
 #Output: Tibble of enrichR results
 do_enrichr <- function(X) {
-  dbs = c("GO_Biological_Process_2021", 
-           "GO_Molecular_Function_2021", 
-           "GO_Cellular_Component_2021")
+  dbs = c("GO_Biological_Process_2023", 
+           "GO_Molecular_Function_2023", 
+           "GO_Cellular_Component_2023")
   
   columns = c("Biological_Process",
                "Molecular_Function",
